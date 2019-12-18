cc30f1a9f73f9ff5c01ae5b46c138b0e0d76fd1c
Ubuntu <ubuntu@ip-172-31-34-122.ec2.internal>
2019-12-18 00:04:28 +0000

    Travis YML file updated into the repo

--- rmdock.sh
+++ rmdock.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 for FILE in `docker images | grep "none"`;do
-`docker rmi $FILE`;
+  if [ $FILE != "juniornsa/todo-*" ]; then
+	  echo "file doesnt have the name on it , must be deleted"
+	  `docker rmi $FILE`
+  fi
 done
